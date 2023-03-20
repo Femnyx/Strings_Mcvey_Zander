e8f0e7e15e9899ec92f0d693ca6a3058c6c2a0c4
Zander <114107976+Femnyx@users.noreply.github.com>
2023-03-20 15:12:23 -0500

    Big String Mega Update v0.1

--- Strings_Mcvey_Zander/Program.cs
+++ Strings_Mcvey_Zander/Program.cs
@@ -6,7 +6,26 @@ namespace Strings_Mcvey_Zander
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Asks user to input a character then would input the character to a variable
+            Console.WriteLine("Please input a character");
+            char myChar = Console.ReadKey().KeyChar;
+
+            // Checks if the character the user inputted is entered
+            bool isLetter = Char.IsLetter(myChar);
+
+            // Prints the results to console
+            Console.WriteLine($"\n {myChar} is a letter: {isLetter}.");
+
+            Console.WriteLine("Why do you like video games?");
+            string sentence = Console.ReadLine();
+
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+
+            bool isIn = sentence.Contains(word);
+
+            Console.WriteLine($"{word} exists in {sentence}, {isIn}!");
+
         }
     }
 }
